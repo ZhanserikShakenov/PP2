a07b5a9e54b3f68218a19a5b729429b96dd22bff
ZhanserikShakenov <shakenov_zhanserik@mail.ru>
2019-01-30 01:10:45 +0600

    I'm a master at Git

---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());                   //input StarTriangle size
+            for (int i = 0; i < n; i++)                              
+            {                                                        
+                for (int c = 0; c <= i; c++)                         
+                    Console.Write("[*]");                            //output the star cyclically
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
