df645e29c89493e0453a55dcbf5ebf7b1bdbc87d
ZhanserikShakenov <shakenov_zhanserik@mail.ru>
2019-01-30 00:52:53 +0600

    I realized

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());                          //declare size of array and input
+            string[] s = Console.ReadLine().Split();                        //input elements of array
+            int[] a = new int[n];                                           //declare array
+            for(int i=0; i<n; i++)                                          
+            {                                                               
+                a[i] = int.Parse(s[i]);                                     //convert to int then add to array
+                Console.Write(a[i] + " " + a[i] + " ");                     //output double each element
+            }
+            Console.ReadKey();
+        }
+    }
+}
